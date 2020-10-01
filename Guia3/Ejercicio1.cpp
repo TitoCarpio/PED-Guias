246ac78cc4853f0d7ac23561ca441264c24eca21
TitoCarpio <00226620@uca.edu.sv>
2020-09-30 20:00:59 -0600

    Guia3

---
+++ Guia3/Ejercicio1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <conio.h>
+#include "Funciones.hpp"
+
+using namespace std;
+
+void menu(Stack *s) {
+    int option;
+    bool status = true;
+    while (status)
+    {
+        cout << "\n" << "***** MENU DE PILAS *****" << endl;
+        cout << "1. Ingresar elemento a la pila" << endl;
+        cout << "2. Eliminar elemento de la pila" << endl;
+        cout << "3. Salir del menu" << endl;
+        cout << "\n" << "Ingrese la opción que deseas ejectuar: ";
+        cin >> option;
+        cout << "\n";
+
+        switch (option)
+        {
+            case 1:
+                int element;
+                cout << "Ingrese el elemento a agregar a la pila: ";
+                cin >> element;
+                push(s, element);
+                cout << "Mostrando la pila actualizada" << endl;
+                showStack(s);
+                break;
+            case 2:
+                pop(s);
+                cout << "Mostrando la pila actualizada" << endl;
+                showStack(s);
+                break;
+            case 3:
+                cout << "Fin del programa" << endl;
+                status = false;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+int main(){
+
+    Stack pila;
+    initiliaze(&pila);
+
+    menu(&pila);
+
+
+
+
+    getch();
+    return 0;
+}
---
+++ Guia3/Funciones.hpp
@@ -0,0 +1,47 @@
+struct Node {
+    int element;
+    struct Node *next;
+};
+typedef struct Node *Stack;
+
+void initiliaze(Stack *s) {
+    *s = NULL ;
+
+}
+
+bool empty(Stack *s) {
+    return *s == NULL;
+}
+
+void push(Stack *s, int e) {
+    struct Node *node = new struct Node;
+   
+    node->element = e;
+    node->next = *s;
+
+    *s = node;
+}
+
+void pop(Stack *s) {
+    if (!empty(s)) {
+        struct Node *aux = *s;
+        *s = (*s)->next;
+        delete(aux);
+    }else {
+        return;
+    }
+}
+
+void showStack(Stack *s) {
+    struct Node *clone = *s;
+    bool isEmpty = false;
+    while (!isEmpty) {
+        if (!empty(&clone))
+        {
+            cout << clone->element << " ";
+            clone = clone->next;
+        }else {
+            isEmpty = true;
+        }
+    }
+}
